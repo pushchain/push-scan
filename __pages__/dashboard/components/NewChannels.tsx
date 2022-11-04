657a54b195c76c820a2dfd7574e40f4d7d1f8280
deekshithepns <deekshith@epns.io>
2022-11-04 20:35:58 +0530

    added leaderboard

---
+++ __pages__/dashboard/components/NewChannels.tsx
@@ -0,0 +1,53 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Box,
+  Grid,
+  Container,
+  Typography,
+  Avatar,
+} from "@mui/material";
+export default function NewChannels() {
+  const data = [
+    { image: "/static/push.svg", name: "Channel1", value: 100 },
+    { image: "/static/push.svg", name: "Channel2", value: 200 },
+    { image: "/static/push.svg", name: "Channel3", value: 300 },
+  ];
+  return (
+    <Grid item xs={12} md={6} lg={6}>
+      <Card sx={{ height: "100%" }}>
+        <CardHeader title="Recently Added" />
+        <CardContent>
+          {data?.map((channel, index) => {
+            return (
+              <Box
+                sx={{
+                  p: 3,
+                  display: "flex",
+                  justifyContent: "space-between",
+                  height: "100%",
+                  width: "100%",
+                }}
+                dir="ltr"
+                key={index}
+              >
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <Typography variant="h5" mr={1}>
+                    {index + 1}.
+                  </Typography>
+                  <Avatar
+                    src={channel.image}
+                    sx={{ width: "26px", height: "26px" }}
+                  />
+                  <Typography variant="h5">{channel.name}</Typography>
+                </Box>
+                <Typography variant="h5">{channel.value}</Typography>
+              </Box>
+            );
+          })}
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
--- __pages__/dashboard/index.tsx
+++ __pages__/dashboard/index.tsx
@@ -1,8 +1,18 @@
+import { Grid, Typography } from "@mui/material";
+import ReactECharts from "echarts-for-react";
 import { DashBoardContainer } from "./dashboard.styled";
+import Topchannels from "./components/TopChannels";
+import NewChannels from "./components/NewChannels";
+
 const DashBoardView = () => {
   return (
     <DashBoardContainer>
-      <h1>Dash</h1>
+      <Typography variant="h3">Push Analytics Dashboard</Typography>
+      <Typography variant="h3" my={3}>LeaderBoard</Typography>
+      <Grid container spacing={4} justifyContent="center">
+        <Topchannels />
+        <NewChannels />
+      </Grid>  
     </DashBoardContainer>
   );
 };
