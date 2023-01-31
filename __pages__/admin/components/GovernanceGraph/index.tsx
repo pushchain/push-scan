c842b6e837d8de00ed7d1e417c03f819091c705a
deekshithmd <deekshithmogra@gmail.com>
2023-01-31 19:42:38 +0530

    graph-fix-5

--- __pages__/admin/components/GovernanceGraph/index.tsx
+++ __pages__/admin/components/GovernanceGraph/index.tsx
@@ -163,7 +163,7 @@ const GovernanceGraph = ({ data, title, label, value, colorSet }: any) => {
               minHeight: '100%',
               minWidth: '100%',
               display: 'flex',
-              justifyContent: 'stretch',
+              justifyContent: 'center',
             }}
             option={getDataPoints({ data, label })}
           />
