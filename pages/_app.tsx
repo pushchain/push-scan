63448cc3d043b0054a5f58a6ee8e16d804700af4
deekshithepns <deekshith@epns.io>
2023-04-26 11:13:32 +0530

    updated imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // React, NextJS imports
 import React from 'react';
+
 // import localFont from '@next/font/local';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -13,31 +14,6 @@ import '../styles/globals.css';
 import Theme from '../theme/Theme';
 import { ThemeProvider as GlobalThemeProvider } from '../contexts/ThemeContext';
 import { DataProvider } from '../contexts/DataContext';
-
-// const strawFord = localFont({
-//   src: [
-//     {
-//       path: './static/fonts/Strawford-Regular.woff2',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './static/fonts/Strawford-Regular.woff',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './static/fonts/Strawford-Regular.eot',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: './static/fonts/Strawford-Regular.ttf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//   ],
-// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
