126ae9181d042aa1dee2e38eca951cd6e7b025a3
rozaso <atheeq@push.org>
2024-09-18 22:10:03 -0700

    setup deployment.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   devIndicators: {
     buildActivity: false,
   },
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
+  },
+  basePath: '/push-analytics-dashboard', // Set this to the repo name if deploying to a subdirectory
+  assetPrefix: '/push-analytics-dashboard', // Ensures assets are served from the correct path
 };
 
 module.exports = nextConfig;
