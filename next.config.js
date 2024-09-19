21e5321982734a182ec8dbd59e2be054207b88c9
rozaso <atheeq@push.org>
2024-09-19 08:18:20 -0700

    Revert back the next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
   swcMinify: true,
   devIndicators: {
     buildActivity: false,
-  },
-  images: {
-    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
-  },
-  basePath: '/push-analytics-dashboard', // Set this to the repo name if deploying to a subdirectory
-  assetPrefix: '/push-analytics-dashboard', // Ensures assets are served from the correct path
+  }
 };
 
 module.exports = nextConfig;
