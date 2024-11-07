9f36431f01f2ff1b4852a865c259b928be1be81f
abhishek-01k <abhishek@push.org>
2024-11-07 13:44:23 +0700

    Fixed the Font style issue

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href='./static/push.png' />
+        <meta
+          name="description"
+          content="push protocol analytics dashboard"
+        />
+        <meta
+          name="keywords"
+          content="Push,Communication,Notification,Chat"
+        />
+        <meta name="author" content="Push Protocol" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
