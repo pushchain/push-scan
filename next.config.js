f86da03d3bad9acda96bf0e7b80c9348b167d3f9
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-12-04 15:11:35 +0530

    base path fix

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
     buildActivity: false,
   },
   // Access the NEXT_PUBLIC_BASE_PATH environment variable
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Default to empty if not defined
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined, // Default to empty if not defined
   // You can also use assetPrefix to set the base path for static assets
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
 };
 
 module.exports = nextConfig;
