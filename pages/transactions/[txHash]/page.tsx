5471ce64f3fc4d42edf4896be2a7ac543469bad1
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-12-09 14:34:28 +0530

    changed the folder strcuture for transaction page

--- pages/transactions/[txHash]/page.tsx
+++ pages/transactions/[txHash]/page.tsx
@@ -4,18 +4,16 @@ import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
 // Internal Components imports
-// import { Spinner } from '../../blocks';
-import Layout from '../../layout';
-import TransactionDetailsView from '../../sections/Transactions/txHash';
+import { Spinner } from '../../blocks';
 
-// const Layout = dynamic(() => import('../../layout'));
+const Layout = dynamic(() => import('../../../layout'));
 
-// const TransactionDetailsView = dynamic(
-//   () => import('../../sections/Transactions/txHash'),
-//   {
-//     loading: () => <Spinner size="extraLarge" />,
-//   }
-// );
+const TransactionDetailsView = dynamic(
+  () => import('../../../sections/Transactions/txHash'),
+  {
+    loading: () => <Spinner size="extraLarge" />,
+  }
+);
 
 const TransactionDetailsPage = () => {
   return (
