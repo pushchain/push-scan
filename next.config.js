c285bae61d7baf1304dafedb5c10cee5b8c97bfd
Riyanshu Patro <riyanshu@push.org>
2025-08-21 19:55:42 +0530

    chore: redirect to donut.push.network

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined, // Default to empty if not defined
   // You can also use assetPrefix to set the base path for static assets
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://donut.push.network',
+        permanent: true, // 308
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
